655c4785855900929eb3521f268609f82962a435
Se-young Hwang <nahwasa@gmail.com>
2022-04-13 19:06:58 +0900

    Add files via upload

---
+++ src/test/java/com/nahwasa/seminar/nahwasaseminar/NahwasaSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.seminar.nahwasaseminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NahwasaSeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
